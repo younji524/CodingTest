174a57cf8313a49a114b628c50fb0f29461ab775
Yunji Kim <76142194+younji524@users.noreply.github.com>
2023-07-27 10:22:48 +0900

    Update CaesarCipher.cpp

--- Programmers/CaesarCipher.cpp
+++ Programmers/CaesarCipher.cpp
@@ -10,6 +10,7 @@ string solution(string s, int n) {
         if (s[i] == ' ') {
             answer += ' ';
         }
+        // a 에서 n이 더해졌을 때 z를 넘어가는 경우를 잘 고려하여야 함
         else if ((s[i] <= 90 && s[i] + n > 90) || (s[i] <= 122 && s[i] + n > 122)) {
             answer += s[i] - 26 + n;
         }
