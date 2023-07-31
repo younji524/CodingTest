cd19a4543c12216875deffad1942fcace1ff825b
Yunji Kim <76142194+younji524@users.noreply.github.com>
2023-07-31 15:26:52 +0900

    Update regex_replace.cpp

--- Tips/regex_replace.cpp
+++ Tips/regex_replace.cpp
@@ -6,11 +6,18 @@ using namespace std;
 
 int main(void)
 {
-    string str = "I'm a strong boy.";
+    string s = "OneTwoThree";
+    string s2 = "...";
 
     // #include <regex> 필요
-    // s = regex_replace(대상 문자열, regex(바꿀 문자열), 치환할 문자열)
-    str = regex_replace(str, regex("boy"), "girl");
+    // 대상 문자열 = regex_replace(대상 문자열, regex(바꿀 문자열), 치환할 문자열)
+    s = regex_replace(s, regex("Three"), "3");
+
+    // {2} : 2개
+    // {2,} : 최소 2개
+    // {2,3} : 2개에서부터 3개까지
+    s2 = regex_replace(s2, regex("[.]{2,}"), ".");
   
-    cout << str; // "I'm a strong girl."
+    cout << s; // "OneTwo3"
+    cout << s2; // "."
 }
