ec6ec2d18a06ad0535be7a5cafabf18b2c46c5f7
Yunji Kim <76142194+younji524@users.noreply.github.com>
2023-08-05 17:14:19 +0900

    Update 카드 뭉치.cpp

--- Programmers/카드 뭉치.cpp
+++ Programmers/카드 뭉치.cpp
@@ -1,39 +1,39 @@
+/*
+idea
+
+goal의 원소들과 카드 뭉치 1과 2의 첫 번째 원소를 비교하고, 일치하는 경우 벡터에서 제외한다.
+
+첫 번째 원소와 일치하지 않는 경우, answer을 no로 정의한다.
+
+첫 번째 원소들로 goal 원소들을 모두 구성할 수 있는 경우, answer을 yes로 정의하는데
+
+1. 카드 뭉치 1과 2의 원소들을 모두 사용해 크기가 둘 다 0인 경우 (cards1.size() + cards2.size() == 0)
+2. goal의 모든 원소들을 구성할 수 있지만 남은 원소가 있는 경우 (cnt == goal.size())
+
+로 구분할 수 있다. 
+*/
+
 #include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
-int find_idx(vector<string> v, string n) {
-    auto it = find(v.begin(), v.end(), n);
-    return it == v.end() ? -1 : it - v.begin();
-}
-
 string solution(vector<string> cards1, vector<string> cards2, vector<string> goal) {
     string answer = "";
-    vector<int> a1;
-    vector<int> a2;
+    int cnt = 0;
     
     for (int i = 0; i < goal.size(); i++) {
-        a1.push_back(find_idx(cards1, goal[i]));
-        a2.push_back(find_idx(cards2, goal[i]));
+        if (cards1[0] == goal[i]) {
+            cards1.erase(cards1.begin());
+            cnt++;
         }
-    if (a1[0] != 0 && a2[0] != 0) answer = "No";
-    
-    for (int j = 0; j < goal.size() - 1; j++) {
-        int gap1 = a1[j + 1] - a1[j];
-        int gap2 = a2[j + 1] - a2[j];
-        
-        if ((a1[j] != -1) && (a1[j + 1] != -1)) {
-            if (0 < gap1 && gap1 < 2) answer = "Yes";
-            else answer = "No";
+        else if (cards2[0] == goal[i]) {
+            cards2.erase(cards2.begin());
+            cnt++;
         }
-        
-        if ((a2[j] != -1) && (a2[j + 1] != -1)) {
-            if (0 < gap2 && gap2 < 2) answer = "Yes";
-            else answer = "No";
-        }       
+        else answer = "No";
     }
+    if ((cards1.size() + cards2.size() == 0) || cnt == goal.size()) answer = "Yes";
     
     return answer;
 }
