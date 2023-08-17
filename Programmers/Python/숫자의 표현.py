da8c407b2e952fb59abb53b4651d32165b32ae93
Yunji Kim <76142194+younji524@users.noreply.github.com>
2023-08-17 17:08:35 +0900

    Update 숫자의 표현.py

--- Programmers/Python/숫자의 표현.py
+++ Programmers/Python/숫자의 표현.py
@@ -1,3 +1,5 @@
+# 투 포인터 이용해서 문제 해결하기
+
 def solution(n):
     answer = 1
     st_idx = 1
@@ -15,5 +17,22 @@ def solution(n):
         else:
             end_idx += 1
             sum += end_idx
+    
+    return answer
+
+# for문 이용해서 문제 해결하기
+
+def solution(n):
+    answer = 0
+    
+    for i in range(1, n + 1):
+        sum = 0
+        for j in range (i, n + 1):
+            sum += j
+            if sum == n:
+                answer += 1
+                break
+            elif sum > n:
+                break
     
     return answer
